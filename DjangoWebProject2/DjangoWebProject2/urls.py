70b2b6c0524373f07e12d4cf97e8ae30c821e702
Николай Лин <nikolylin@mail.ru>
2021-06-06 02:35:29 +0300

    f

--- DjangoWebProject2/DjangoWebProject2/urls.py
+++ DjangoWebProject2/DjangoWebProject2/urls.py
@@ -38,7 +38,7 @@ urlpatterns = [
     url(r'^registration$', app.views.registration, name='registration'),
     
     url(r'^login/$',
-        django.contrib.auth.views.login,
+        django.contrib.auth.views.LoginView,
         {
             'template_name': 'app/login.html',
             'authentication_form': app.forms.BootstrapAuthenticationForm,
@@ -50,7 +50,7 @@ urlpatterns = [
         },
         name='login'),
     url(r'^logout$',
-        django.contrib.auth.views.logout,
+        django.contrib.auth.views.LogoutView,
         {
             'next_page': '/',
         },
